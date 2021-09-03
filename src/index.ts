b737c22568cac9ca224eb71c8103a6a15581371e
Thomas Guillouët <thomas.guillouet@outlook.com>
2021-09-03 16:59:44 +0200

    Basic typescript with eslint setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Here")
+import { Client, Intents } from "discord.js";
+import { token } from "./configs/discord-config.json";
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.on("ready", () => {
+	console.log("The discord bot is ready");
+});
+
+client.login(token);
