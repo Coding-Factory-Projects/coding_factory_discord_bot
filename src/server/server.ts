043ca9e3d033dec3a6f63bc7be6bfa6435d3fcce
Thomas Guillouët <thomas.guillouet@outlook.com>
2021-09-11 19:40:01 +0200

    Finishing role assignment

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,28 +1,39 @@
 import * as express from "express";
 import * as path from "path";
 import { logger } from "./../loggers/logger";
-import { guildId } from "./../configs/discord-config.json";
+import { guildId, baseRoleId, guestRoleId } from "./../configs/discord-config.json";
 import { port } from "./../configs/server-config.json";
 import { client } from "./../index";
 
 const app = express();
+app.use(express.json());
 
 app.get("/oauth2/redirect", (request: express.Request, response: express.Response) => {
   response.sendFile(path.join(process.cwd(), "public", "index.html"));
 });
 
-app.get("/change-nickname", async (request: express.Request, response: express.Response) => {
+app.post("/change-status", async (request: express.Request, response: express.Response) => {
   const guild = await client.guilds.fetch(guildId);
   if (!guild) {
     response.status(404).json({ success: false, message: "Le serveur spécifié n'existe pas" });
     return;
   }
 
-  const { userId } = request.query as { userId: string };
-  const user = await guild.members.fetch(userId);
-  user.setNickname("Test nickname");
+  const role = await guild.roles.fetch(baseRoleId);
+  const guestRole = await guild.roles.fetch(guestRoleId);
+  if (!guild) {
+    response.status(404).json({ success: false, message: "Les rôle spécifiés dans la configuration n'existent pas" });
+    return;
+  }
 
-  response.json({ success: true, message: "Votre nom à bien été changé sur le serveur" });
+  const { userId, fullname } = request.body;
+
+  const user = await guild.members.fetch(userId);
+  await user.setNickname(fullname);
+  await user.roles.remove(guestRole);
+  await user.roles.add(role);
+
+  response.json({ success: true });
 });
 
 app.listen(port, () => {
