66d48732436c6fc47af4d0572a09913340b606ce
Thomas Guillouët <thomas.guillouet@outlook.com>
2024-09-22 12:30:09 +0200

    Add the createcategory command
    It is a copy of the /createcategories command with a parameter to
    specify the name (and of course only one category will be created)

---
+++ src/commands/create-category.ts
@@ -0,0 +1,61 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { MessageComponentInteraction, Permissions } from "discord.js";
+import { ICommand } from "ICommand";
+import { logger } from "./../loggers/logger";
+import { everyoneRoleId, productOwnersRoleId, channels } from "./../configs/channels.json";
+import { adminRoleId } from "./../configs/discord-config";
+
+const createCategoryCommand: ICommand = {
+  name: "createcategory",
+  roles: [adminRoleId],
+  commandBuilder: new SlashCommandBuilder()
+    .setName("createcategory")
+    .setDescription("Crée une nouvelle catégorie (Admin)")
+    .addStringOption((option) =>
+      option.setName("name").setDescription("Nom de la catégorie").setRequired(true)
+    ),
+  execute: async (interaction: MessageComponentInteraction) => {
+    logger.info("Creating all the channels...");
+    await interaction.reply("Création des channels...");
+    try {
+      const categoryId = interaction.options.getString("name");
+      logger.info(`Creating the category "${role.role}"`);
+      // Create the category
+      const createdCategory = await interaction.guild.channels.create(role.role, {
+        type: 4,
+        permissionOverwrites: [
+          {
+            id: everyoneRoleId,
+            deny: [Permissions.FLAGS.VIEW_CHANNEL],
+          },
+          {
+            id: productOwnersRoleId,
+            allow: [Permissions.FLAGS.VIEW_CHANNEL],
+          },
+          {
+            id: role.id,
+            allow: [Permissions.FLAGS.VIEW_CHANNEL],
+          },
+        ],
+      });
+
+      // Create all the channels in the category
+      for (const channel of channels) {
+        logger.info(`Creating the channel ${channel.name} for the category`);
+        if (channel.type === "text")
+          await interaction.guild.channels.create(channel.name, { type: 0, parent: createdCategory.id });
+        else if (channel.type === "voice")
+          await interaction.guild.channels.create(channel.name, { type: 2, parent: createdCategory.id });
+      }
+      logger.info(`All the channels are created for the category ${role.role}`);
+
+      await interaction.editReply("Les catégories viennent d'être créés");
+      logger.info("All the categories are now created !");
+    } catch (e) {
+      logger.error(JSON.stringify(e));
+      await interaction.editReply("Une erreur s'est produite !");
+    }
+  },
+};
+
+export default createCategoryCommand;
